dc4eef5afa2074572d15483f77cee45a6bc9d7f7
Victor-Vaglieri <victor_vglr@hotmail.com>
2026-02-02 09:06:28 -0300

    Adição dos layouts da pagina de edição e criação de metodo de pagamento

--- app/src/main/java/com/example/controledovitao/ui/PaymentsActivity.kt
+++ app/src/main/java/com/example/controledovitao/ui/PaymentsActivity.kt
@@ -1,4 +1,6 @@
 package com.example.controledovitao.ui
 
+// TODO tudo aqui, precisa iniciar em payment_method_home caso clicar em editar ir para payment_method_list e caso para criar ir para payment_method_create
+// o do payment_method_list terá a lista de todos os metodos e ao clicar em um vai entrar payment_method_edit com as infos do metodo selecionado
 class PaymentsActivity {
 }
